03ffecbee8982e4ff70eabce72a0ec9aa3aac6e0
tyliaitpk <tyliaitpk@gmail.com>
2026-01-14 21:21:50 +0200

    testas13

--- 00-testas-260111/script.js
+++ 00-testas-260111/script.js
@@ -1,2 +1,11 @@
 // Ši eilutė suranda elementą su ID "metai" ir įrašo į jį dabartinius metus
-    document.getElementById("metai").textContent = new Date().getFullYear();
+document.getElementById('metai').textContent = new Date().getFullYear();
+
+// Ši funkcija atnaujina progreso juostą, kai vartotojas slenka puslapiu
+//     window.onscroll = function() {
+//     let langas = document.documentElement;
+//     let virsus = langas.scrollTop;
+//     let aukstis = langas.scrollHeight - langas.clientHeight;
+//     let procentas = (virsus / aukstis) * 100;
+//     document.getElementById("progresas").style.width = procentas + "%";
+// };
